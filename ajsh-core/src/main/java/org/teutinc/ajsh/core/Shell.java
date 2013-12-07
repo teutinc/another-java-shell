2ee2d84e465ee505086f492a458f470f8b70ba4d
apeyrard <augustin.peyrard@gmail.com>
2013-12-07 02:27:05 +0100

    Add the main Shell class

---
+++ ajsh-core/src/main/java/org/teutinc/ajsh/core/Shell.java
@@ -0,0 +1,12 @@
+package org.teutinc.ajsh.core;
+
+/**
+ * The ajsh shell class.
+ *
+ * @author apeyrard
+ */
+public class Shell {
+	public static void main(String[] args) {
+		System.out.println("this is a simple test...");
+	}
+}
